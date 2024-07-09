3a8c054db27a887c2d749869561d1f3e0a8482f1
KatoMasami <163547645+katomasami@users.noreply.github.com>
2024-07-09 17:57:44 +0000

    [Finish] 4-19

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -19,7 +19,7 @@ class PostImagesController < ApplicationController
     @post_comment = PostComment.new
   end
   
-   def destroy
+  def destroy
     post_image = PostImage.find(params[:id])
     post_image.destroy
     redirect_to post_images_path
